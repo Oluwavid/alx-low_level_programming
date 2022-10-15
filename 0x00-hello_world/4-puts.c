31ef7787481bc13b9cd43e87819d37418d9aed19
Oluwavid <dhenyoluwa@gmail.com>
2022-10-15 03:54:21 +0100

    Task 4. First C program with betty coding style

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+*Main function for the program*/ int main(void)
+{
+	char mystr[] = "Programming is like building a multilingual puzzle";
+
+	puts(mystr);
+
+	return (0);
+}
