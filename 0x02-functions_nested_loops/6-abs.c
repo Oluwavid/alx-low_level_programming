440bff9423a016563e77971147e0f5fe878b8544
Oluwavid <dhenyoluwa@gmail.com>
2022-10-19 04:19:51 +0100

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *_abs - main function of the program.
+ *@n: integer variable for the function.
+ *
+ *Return: return values based on the condition.
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
