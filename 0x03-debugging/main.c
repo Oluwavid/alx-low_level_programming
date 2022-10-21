d195d7ffcf400a3460a3d30daffb940ebb0abfa3
Oluwavid <dhenyoluwa@gmail.com>
2022-10-21 01:15:06 +0100

    Debugging Task 1

---
+++ 0x03-debugging/main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *main - Main function of the program.
+ *
+ *Return: Return 0 if program is successful.
+ */
+int main(void)
+{
+	int i;
+
+	i = 24;
+	positive_or_negative(i);
+
+	return (0);
+}
